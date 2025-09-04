bc56c433eaa80f483b9d543b3f61bccb291fe8ff
SidSag19 <sandeepkumar8168@gmail.com>
2025-09-04 17:16:26 +0530

    Add files via upload

---
+++ fibrecur.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int fib(int n);
+
+int main(){
+    int num;
+    printf("Enter number nth fibonacci element");
+    scanf("%d", &num);
+    int res;
+    res=fib(num);
+    printf("%dth fibonacci elemnt is %d",num,res);
+    return 0;
+}
+
+int fib(int n){
+    if (n==1){
+        return 0;}
+    else if (n==2){
+        return 1;}
+    else {
+    return fib(n-1)+fib(n-2);}
+}
